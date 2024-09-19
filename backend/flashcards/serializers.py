db3c14589d0fdef9dae1c2001dde7d4e3eb331fe
Claire Dochez <clairedochez@gmail.com>
2024-09-19 16:12:41 +0200

    ✨ Create serializers and implement API viewsets for Deck and Card models

---
+++ backend/flashcards/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from flashcards.models import Card, Deck
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['question', 'answer', 'creation_date', 'reviewed_date', 'deck', 'score']
+
+class DeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = ['name']
