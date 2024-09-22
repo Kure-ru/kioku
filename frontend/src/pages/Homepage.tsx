7b213fbc636965a70bdf89c3c5fa14191e955517
Claire Dochez <clairedochez@gmail.com>
2024-09-22 08:44:21 +0200

    🔒 Connect form to backend endpoint for deck creation, add permissions

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,8 +1,9 @@
 import { Container, Title, List, Anchor, Flex, Button, Dialog, Group, TextInput, ActionIcon } from "@mantine/core"
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Deck } from "../Types";
 import { useDisclosure } from "@mantine/hooks";
 import { IoAdd } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 interface DeckListProps {
     decks: Deck[];
@@ -11,24 +12,43 @@ interface DeckListProps {
 interface DeckDialogProps {
     opened: boolean;
     close: () => void;
+    onDeckAdded: (deck: Deck) => void;
 }
+
+
 
 export const Homepage = () => {
     const [decks, setDecks] = useState<Deck[]>([]);
     const [opened, { toggle, close }] = useDisclosure(false);
+    const navigate = useNavigate();
+
+    const fetchDecks = async () => {
+        const token = localStorage.getItem('accessToken');
+        try {
+            const response = await fetch('http://127.0.0.1:8000/decks/', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            if (response.status === 401) {
+                navigate('/login')
+                return;
+            }
+            const data = await response.json();
+            setDecks(data);
+        } catch (error) {
+            console.error('Error when fetching the decks', error)
+        }
+    }
 
     useEffect(() => {
-        const fetchDecks = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:8000/decks/');
-                const data = await response.json();
-                setDecks(data);
-            } catch (error) {
-                console.error('Error when fetching the decks', error)
-            }
-        }
         fetchDecks();
     }, []);
+
+    const handleDeckAdded = (newDeck: Deck) => {
+        setDecks((prevDecks) => [...prevDecks, newDeck]);
+    }
 
     return (
         <Container>
@@ -36,7 +56,7 @@ export const Homepage = () => {
                 <Title order={2}>decks</Title>
                 <Button onClick={toggle}>new</Button>
             </Flex>
-            <DeckDialog opened={opened} close={close} />
+            <DeckDialog opened={opened} close={close} onDeckAdded={handleDeckAdded} />
             <DeckList decks={decks} />
         </Container >
     )
@@ -52,26 +72,53 @@ const DeckList: React.FC<DeckListProps> = ({ decks }) => (
     </List>
 )
 
-const DeckDialog: React.FC<DeckDialogProps> = ({ opened, close }) => {
+const DeckDialog: React.FC<DeckDialogProps> = ({ opened, close, onDeckAdded }) => {
     const [deckName, setDeckName] = useState<string>("");
     const [error, setError] = useState<string>("");
 
-    const handleAddDeck = () => {
+    const handleAddDeck = async () => {
         if (!deckName.trim()) {
             setError("Deck name is required.")
         }
         else {
-            console.log("New deck", deckName)
-            setDeckName("")
-            setError("")
-            close()
+            try {
+                const token = localStorage.getItem('accessToken');
+                const response = await fetch('http://127.0.0.1:8000/decks/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                    body: JSON.stringify({ name: deckName })
+                });
+
+                if (!response.ok) {
+                    setError("Deck name is required.")
+                    throw new Error('Failed to create new deck.');
+                }
+
+                const newDeck = await response.json();
+                onDeckAdded(newDeck);
+                setDeckName("")
+                setError("")
+                close()
+            } catch (error) {
+                console.error('Error when creating the deck', error);
+                setError('Failed to create deck, please try again.');
+            }
         }
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setDeckName(e.target.value)
+        setError('');
     }
 
     return (
         <Dialog position={{ top: 60, right: 250 }} opened={opened} withCloseButton onClose={close}>
             <Group>
-                <TextInput style={{ width: '75%' }} placeholder="new deck name" onChange={(e) => setDeckName(e.target.value)} error={error ? error : null} />
+                <TextInput style={{ width: '75%' }} placeholder="new deck name" onChange={(e) => handleChange(e)} error={error ? error : null}
+                />
                 <ActionIcon onClick={handleAddDeck} variant="filled" aria-label="Add deck"><IoAdd /></ActionIcon>
             </Group>
         </Dialog>
