6042bc1cf52b18647943c4d138c0d26235ba6b8f
Claire Dochez <clairedochez@gmail.com>
2024-09-30 19:45:13 +0200

    💄 improve header

--- frontend/src/Components/AppLayout.tsx
+++ frontend/src/Components/AppLayout.tsx
@@ -1,25 +1,72 @@
-import { AppShell, Flex, Title, Button } from '@mantine/core';
+import { AppShell, Image, UnstyledButton, Container, Group, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
+import classes from './AppLayout.module.css';
+import logo from '../assets/logo.jpg';
+
+
+const links = [
+    { link: '/', label: 'decks' },
+    { link: 'browse', label: 'browse' },
+    { link: 'stats', label: 'stats' },
+];
+
+const authLinks = [
+    { link: '/login', label: 'Login' },
+    { link: '/signup', label: 'Sign up' },
+]
 
 const Layout = () => {
+    const [opened, { toggle }] = useDisclosure(false);
+    const [active, setActive] = useState(links[0].link);
     const navigate = useNavigate();
+
+    const isAuthenticated = !!localStorage.getItem('accessToken');
 
     const handleLogout = () => {
         localStorage.removeItem('accessToken');
         navigate('/login');
     };
 
+    const items = isAuthenticated
+        ? links.map((link) => (
+            <Link
+                key={link.label}
+                to={link.link}
+                className={classes.link}
+                data-active={active === link.link || undefined}
+                onClick={() => setActive(link.link)}
+            >
+                {link.label}
+            </Link>
+        ))
+        : authLinks.map((link) => (
+            <Link
+                key={link.label}
+                to={link.link}
+                className={classes.link}
+                data-active={active === link.link || undefined}
+                onClick={() => setActive(link.link)}
+            >
+                {link.label}
+            </Link>
+        ));
+
     return (
         <AppShell
             header={{ height: 60 }}
             padding="md"
         >
-            <AppShell.Header>
-                <Flex justify="space-between" align="center" style={{ width: '100%' }}>
-                    <Title onClick={() => navigate('/')} order={1}>kioku</Title>
-                    <Link to='browse'>browse</Link>
-                    <Button onClick={handleLogout}>Logout</Button>
-                </Flex>
+            <AppShell.Header className={classes.header}>
+                <Container className={classes.inner}>
+                    <Image src={logo} style={{ width: '100px' }} />
+                    <Group gap={5} visibleFrom="xs">
+                        {items}
+                        {isAuthenticated && <UnstyledButton className={classes.link} onClick={handleLogout}>logout</UnstyledButton>}
+                    </Group>
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+                </Container>
             </AppShell.Header>
             <AppShell.Main>
                 <Outlet />
